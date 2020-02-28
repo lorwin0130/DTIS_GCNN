ad35b8e2805290bed3bedf79f488832e5480e3fd
wzx <806935171@qq.com>
2020-02-28 22:50:35 +0800

    2020.02.28

---
+++ pickle_in.py
@@ -0,0 +1,35 @@
+from NeuralGraph.processing import data_parser, pd_to_pickle
+from NeuralGraph.util import Timer
+
+
+def lst_div(lst, batch_size=1000):
+    lst = [lst[i:i+batch_size] for i in range(0, len(lst), batch_size)]
+    return lst
+
+
+def main():
+    data_path = '/home/ubuntu/wangzhongxu/gcnn2/NGFP/dataset'
+    save_dir = '/home/ubuntu/wangzhongxu/gcnn2/NGFP/dataset/pickle'
+    PD_FILE = 'pd_test.txt' # pre_data_all.txt
+    BATCH_SIZE = 6 # 1000
+    MAX_DEGREE = 6
+    MAX_ATOMS = 80
+    now, start = 0, 0
+
+    pd_all_lst = data_parser(data_path, pd_filename=PD_FILE)
+    all_size = len(pd_all_lst)
+    pd_all_lst = lst_div(pd_all_lst, batch_size=BATCH_SIZE)
+    batch_size = len(pd_all_lst)
+    print('\nprocessing start: data length is {}\nfile save path: {}\n'.format(all_size, save_dir))
+    for pd_lst in pd_all_lst:
+        if now<start: now+=1;continue
+        begin_idx, end_idx = now*BATCH_SIZE+1, now*BATCH_SIZE+len(pd_lst)
+        save_file = '{}/pd_lst_{}_{}-{}'.format(save_dir, now, begin_idx, end_idx)
+        with Timer() as t:
+            pd_to_pickle(save_file, pd_lst, data_path, max_degree=MAX_DEGREE, max_atoms=MAX_ATOMS)
+            print('processing: {}/{} ({}-{} of {})'.format(now, batch_size, begin_idx, end_idx, all_size))
+        now+=1
+
+
+if __name__ == '__main__':
+    main()
--- pickle_out.py
+++ pickle_out.py
@@ -14,14 +14,15 @@ def main():
     start, amount = 0, 4
     input_lst = []
 
-    dir_lst = os.listdir(save_dir)
-    dir_lst.sort(key=str_key)
-    for file in dir_lst[start:start+amount]:
-        file = '{}/{}'.format(save_dir, file)
-        print(file)
-        input_lst += pickle_to_input(file)
-    out = lst_to_out(input_lst)
-    print(out[0].shape)
+    with Timer() as t:
+        dir_lst = os.listdir(save_dir)
+        dir_lst.sort(key=str_key)
+        for file in dir_lst[start:start+amount]:
+            file = '{}/{}'.format(save_dir, file)
+            # print(file)
+            input_lst += pickle_to_input(file)
+        out = lst_to_out(input_lst)
+        print(out[0].shape)
 
 
 if __name__ == '__main__':
