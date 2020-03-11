e69c695bf9071963d06d4c53796e0723757cdfe0
wzx <806935171@qq.com>
2020-03-11 18:28:52 +0800

    0311

--- NeuralGraph/model.py
+++ NeuralGraph/model.py
@@ -47,9 +47,11 @@ class QSAR(nn.Module):
         self.gcn_pro_2 = GraphConv_p(input_dim=200, conv_width=100)
         self.gop = GraphOutput(input_dim=134, output_dim=hid_dim_m)
         self.gop_p = GraphOutput_p(input_dim=100, output_dim=hid_dim_p)
-        # self.bn = nn.BatchNorm2d(80)
+        self.bn = nn.BatchNorm2d(80)
         self.pool = GraphPool()
         self.fc1 = nn.Linear(hid_dim_m + hid_dim_p, 100)
+        #self.fc1 = nn.Linear(hid_dim_p , 100)
+        self.fc3 = nn.Linear(100, 100)
         self.fc2 = nn.Linear(100, n_class)
         self.to(dev)
 
@@ -68,13 +70,19 @@ class QSAR(nn.Module):
         # for pocket
         p_atoms = self.gcn_pro_1(p_atoms, p_edges)
         p_atoms = F.relu(p_atoms)
+        # p_atoms = self.bn(p_atoms)
         p_atoms = self.gcn_pro_2(p_atoms, p_edges)
         p_atoms = F.relu(p_atoms)
+        # p_atoms = self.bn(p_atoms)
         fp_p = self.gop_p(p_atoms, p_edges)
 
         fp = T.cat([fp_m, fp_p], dim=1)
-        interaction = T.sigmoid(self.fc1(fp))
-        out = T.softmax(self.fc2(interaction),dim=1)
+        # interaction = T.sigmoid(self.fc1(fp))
+        # out = T.softmax(self.fc2(interaction),dim=1)
+        tmp = self.fc1(fp)
+        
+        tmp1 = self.fc3(tmp)
+        out = F.sigmoid(self.fc2(tmp1))
         return out
 
     def fit(self, loader_train, loader_valid, path, epochs=1000, early_stop=100, lr=1e-3):
@@ -89,16 +97,22 @@ class QSAR(nn.Module):
             for Ab, Bb, Eb, Nb, Vb, yb in loader_train:
                 Ab, Bb, Eb, Nb, Vb, yb = Ab.to(dev), Bb.to(dev), Eb.to(dev), Nb.to(dev), Vb.to(dev), yb.to(dev)
                 optimizer.zero_grad()
-                y_ = self.forward(Ab, Bb, Eb, Nb, Vb)
-                loss = criterion(T.max(y_,1).values, yb.view(-1))
+                print("yb:",yb)
+                #print('Vb:',Vb)
+                y_ = self.forward(Ab, Bb, Eb, Nb/300.0, Vb/30.0)
+                print("y_:",y_)
+                ix = yb == yb
+                yb, y_ = yb[ix], y_[ix]
+                loss = criterion(y_, yb)
+                print('loss:',loss.item())
                 loss_train += loss
-                acc_train += (y_.argmax(dim=1).float()==yb.view(-1)).float().mean().item()
+                acc_train += 0#(y_.argmax(dim=1).float()==yb.view(-1)).float().mean().item()
                 loss.backward()
                 optimizer.step()
             loss_train, acc_train = loss_train/len(loader_train), acc_train/len(loader_train)
             loss_valid, acc_valid = self.evaluate(loader_valid)
             # print('[Epoch:%d/%d] %.1fs loss_train: ??? loss_valid: %f' % (epoch, epochs, time.time() - t0, loss_valid))
-            print('[Epoch:%d/%d] %.1fs loss_train: %.3f loss_valid: %.3f acc_train: %.3f acc_valid: %.3f' % (epoch, epochs, time.time() - t0, loss_train, loss_valid, acc_train, acc_valid))
+            print('[Epoch:%d/%d] %.1fs loss_train: %.3f loss_valid: %.3f acc_train: %.3f acc_valid: %.3f' % (epoch, epochs, time.time() - t0, loss.item(), loss_valid, acc_train, acc_valid))
             if loss_valid < best_loss:
                 T.save(self, path + '.pkg')
                 print('[Performance] loss_valid is improved from %f to %f, Save model to %s' % (best_loss, loss_valid, path + '.pkg'))
@@ -115,8 +129,10 @@ class QSAR(nn.Module):
         for Ab, Bb, Eb, Nb, Vb, yb in loader:
             Ab, Bb, Eb, Nb, Vb, yb = Ab.to(dev), Bb.to(dev), Eb.to(dev), Nb.to(dev), Vb.to(dev), yb.to(dev)
             y_ = self.forward(Ab, Bb, Eb, Nb, Vb)
-            loss += criterion(T.max(y_,1).values, yb.view(-1)).item()
-            acc += (y_.argmax(dim=1).float()==yb.view(-1)).float().mean().item()
+            ix = yb == yb
+            yb, y_ = yb[ix], y_[ix]
+            loss += criterion(y_, yb).item()
+            acc += 0#(y_.argmax(dim=1).float()==yb.view(-1)).float().mean().item()
             # acc_tensor = (y_.argmax(dim=1).float().cpu()==yb.view(-1).cpu())
             # print('a:',Counter(acc_tensor.numpy()))
             # print('b:',Counter(y_.argmax(dim=1).float().cpu().numpy().tolist()))
--- test.py
+++ test.py
@@ -7,11 +7,10 @@ from NeuralGraph.pickle_out import pickle_out
 from collections import Counter
 import numpy as np
 
-
 def main():
     print('\nTRAIN START!!!')
     with Timer() as t2:
-        train_set, valid_set = pickle_out(start=0, amount=1, random_state=None)
+        train_set, valid_set = pickle_out(start=0, amount=5, random_state=None)
         # print(train_set[5].shape)
         # print(valid_set[5].shape)
         print('train:',Counter(train_set[5].view(-1).cpu().numpy().tolist()))
@@ -21,19 +20,24 @@ def main():
 
     with Timer() as t3:
         print(len(train_set), len(valid_set))
-        train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
+        for i in range(40):
+            # print(train_set[i][3].max())
+            print(train_set[i][0].max(),train_set[i][1].max(),train_set[i][2].max(),train_set[i][3].max(),train_set[i][4].max(),train_set[i][5].max())
+            # print(train_set[i][0],train_set[i][1],train_set[i][2],train_set[i][3],train_set[i][4],train_set[i][5])
+        t_set = [train_set[i] for i in range(8)]
+        train_loader = DataLoader(t_set, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
         valid_loader = DataLoader(valid_set, batch_size=BATCH_SIZE)
         print('data load:')
 
     with Timer() as t4:
         # net = GraphConvAutoEncoder(hid_dim_m=128, hid_dim_p=128, n_class=2)
         # net = net.fit(train_loader, epochs=100)
-        net = QSAR(hid_dim_m=216, hid_dim_p=512, n_class=2)
+        net = QSAR(hid_dim_m=216, hid_dim_p=512, n_class=1)
         net = net.fit(train_loader, valid_loader, epochs=N_EPOCH, path='output/gcnn')
         print('model:')
 
 
 if __name__ == '__main__':
-    BATCH_SIZE = 128 # 128
+    BATCH_SIZE = 1 # 128
     N_EPOCH = 200
     main()
